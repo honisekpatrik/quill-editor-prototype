b873b0cf469804b1acaef06acdf29ec6245b9202
honisekpatrik <patrik.honisek565@gmail.com>
2020-02-09 15:06:07 +0100

    Copied from local solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent implements OnInit {
+  title = 'bp-prototype2';
+
+  editorForm: FormGroup;
+
+  content: string;
+
+  ngOnInit(): void {
+    this.editorForm = new FormGroup({editor: new FormControl(null)});
+  }
+
+  onSubmit() {
+    this.content = this.editorForm.get('editor').value;
+    console.log(this.editorForm.get('editor').value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { QuillModule } from 'ngx-quill'
+import {ReactiveFormsModule} from '@angular/forms';
 
+import {NoSanitizePipe} from './no-sanitize-pipe.pipe';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, QuillModule.forRoot(), FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, QuillModule.forRoot(), FormsModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, NoSanitizePipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
